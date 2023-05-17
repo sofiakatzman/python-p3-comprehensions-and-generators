0ddfb4e564bd7db81fa5b35fa9b58bc36d4dc004
Sofia <katzmansof@gmail.com>
2023-05-17 14:03:36 -0400

    finished lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [n for n in num_list if n%2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    return [n + "!" for n in sentence_list]
+
+
+# new_list = [optional_operation(item) for item in old_list if optional_condition == True]``
